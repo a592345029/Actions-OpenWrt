e66e06a685dfcca98505c7411914dc91efcb29bd
iplcdn <58213711+iplcdn@users.noreply.github.com>
2021-01-11 01:18:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,7 @@ sed -i 's/OpenWrt/iplcdn/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /iplcdn build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
+rm -rf package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
