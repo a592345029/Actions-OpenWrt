ee400cd9cc3e0ca059726696086d5690e27df24c
iplcdn <58213711+iplcdn@users.noreply.github.com>
2021-01-11 18:50:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,8 +20,8 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 sed -i "s/OpenWrt /complied by iplcdn $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
-rm -rf package/lean/default-settings/files/zzz-default-settings
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/99222f49fd9f63eef575ac34f251280e6e97f3b3/package/lean/default-settings/files/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
+#rm -rf package/lean/default-settings/files/zzz-default-settings
+#wget https://raw.githubusercontent.com/coolsnowwolf/lede/99222f49fd9f63eef575ac34f251280e6e97f3b3/package/lean/default-settings/files/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
 sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'         package/lean/autosamba/Makefile
 sed -i '92d'                                             package/system/opkg/Makefile
 sed -i '/shadow/d'                                       package/lean/default-settings/files/zzz-default-settings
@@ -36,14 +36,14 @@ sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all'
 sed -i "45i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/x86_64' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
 
 # Delete default password
-sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
 #rm -rf feeds/packages/net/smartdns
 rm -rf package/lean/luci-theme-argon
 
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome                           package/adg
 #git clone https://github.com/pymumu/luci-app-smartdns.git -b lede                                         package/luci-app-smartdns
-#svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns    
+#svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns    提示缺少依赖smartdns
 
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
