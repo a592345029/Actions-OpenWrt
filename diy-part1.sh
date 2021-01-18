de35adc61e5c62e003bbebdf56b72e8314ea0969
iplcdn <58213711+iplcdn@users.noreply.github.com>
2021-01-18 15:35:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,21 +15,21 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '$a src-git jerrykuku https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-#sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
-#sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git jerrykuku https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
 
 #====================================================================
 #添加额外软件包
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06          
-git clone https://github.com/jerrykuku/openwrt-package.git package/jerrykuku-package
+#git clone https://github.com/jerrykuku/openwrt-package.git package/jerrykuku-package
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8-packages
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
-git clone https://github.com/vernesong/OpenClash.git -b master package/OpenClash
+#git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/tindy2013/openwrt-subconverter.git package/subconverter
 git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git package/xiaoqingfeng
 
