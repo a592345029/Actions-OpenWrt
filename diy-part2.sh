5502e9e4185873d8e19c050dbdc6da9c097bcd4d
insChow <58213711+insChow@users.noreply.github.com>
2021-02-16 12:37:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/iplcdn/Actions-OpenWrt
+# https://github.com/insChow/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #====================================================================
