addfed543e209b47e6747ce89b9de541d3fb92b5
insChow <58213711+insChow@users.noreply.github.com>
2021-02-25 09:00:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,6 +25,6 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
 git clone https://github.com/tindy2013/openwrt-subconverter.git package/subconverter
 git clone https://github.com/esirplayground/luci-app-LingTiGameAcc.git package/luci-app-LingTiGameAcc
-git clone ttps://github.com/esirplayground/LingTiGameAcc.git package/LingTiGameAcc
+git clone https://github.com/esirplayground/LingTiGameAcc.git package/LingTiGameAcc
 
 #===============================================================================================
